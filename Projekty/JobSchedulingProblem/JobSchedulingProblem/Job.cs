6eb3ade046fb8307d85d0ae5b75f0ca9269701cb
michalbuszka <318049@stud.umk.pl>
2025-05-11 18:13:54 +0200

    Job Scheduling init

---
+++ Projekty/JobSchedulingProblem/JobSchedulingProblem/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSchedulingProblem
+{
+    public class Job
+    {
+        public int timeDuration { get; set; }
+        public int k { get; set; } //indeks procesora
+        public float startTime { get; set; }
+        public float endTime { get; set; }
+    }
+}
---
+++ Projekty/JobSchedulingProblem/JobSchedulingProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    public static class Program 
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
