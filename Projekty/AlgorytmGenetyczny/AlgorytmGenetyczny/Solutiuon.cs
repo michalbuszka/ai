a2017b290b86a29c3f28cc79be4e7dafe4eab6fd
michalbuszka <318049@stud.umk.pl>
2025-05-20 00:11:34 +0200

    Algorytm genetyczny v1

---
+++ Projekty/AlgorytmGenetyczny/AlgorytmGenetyczny/Solutiuon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmGenetyczny
+{
+    public class Solutiuon
+    {
+        private int[] Geny;
+        public int getFitness ()
+        {
+            int counter = 0;
+            foreach (int gen in Geny)
+            {
+                counter += gen;
+            }
+            return counter;
+        }
+        public int Fitness { 
+            get 
+            {
+                return getFitness(); 
+            }
+        }    
+        public Solutiuon(int ilośćGenów) 
+        {
+            Geny = new int[ilośćGenów];
+        }
+
+    }
+}
